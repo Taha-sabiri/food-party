8564b0e01cdaca5a2c0ca69346f6ae04b051c864
taha <t.sabiri>
2025-10-26 08:42:18 +0330

    push

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Vazirmatn } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const rubik = Vazirmatn({
   variable: "--font-geist-sans",
@@ -49,6 +50,7 @@ export default function RootLayout({
         className={`${rubik.variable}  antialiased  w-screen h-screen flex justify-center items-start  `}
       >
         <Toaster position="top-center" richColors />
+        <SpeedInsights/>
         {children}
 
 
